074afc71e3476da2f01983be1588446d996d527a
chenDary <10444087+chendary@user.noreply.gitee.com>
2022-11-10 21:52:42 +0800

    等待尝试

--- UnityServer/StartProcess.go
+++ UnityServer/StartProcess.go
@@ -2,6 +2,7 @@ package UnityServer
 
 import (
 	"MasterClient/Logs"
+	"os"
 	"os/exec"
 )
 
@@ -31,7 +32,7 @@ func StartAnalyze(data AnalyzeData) {
 	}
 }
 
-func TestRun() {
+func Test2() {
 	Unity_Name := "F:/Unity2021.3.2f1/Unity.exe " //需要启动的程序名,Unity.exe的具体目录
 	argu := "-quit -batchmode -nographics "
 	argu = argu + "-projectPath " + config.UnityProjectPath + " "
@@ -45,11 +46,21 @@ func TestRun() {
 	argu = argu + "-funhashPath E:/TestFiles/1668009600raw_funhash.json "
 	argu = argu + "-shieldSwitch false "
 	argu = Unity_Name + argu
-	cmd := exec.Command("cmd.exe", "/c", "start "+argu)
-	er := cmd.Start()
-	if er != nil { // 运行命令
-		Logs.Loggers().Print(er)
+	TestRun(argu)
+}
+
+func TestRun(argu string) {
+	for {
+		cmd := exec.Command("cmd.exe", "/c", "start "+argu)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		er := cmd.Start()
+		if er != nil { // 运行命令
+			Logs.Loggers().Println("start oasis erir [-] Error: ", os.Stderr)
+			Logs.Loggers().Print(er)
+		}
+		Logs.Loggers().Println("启动成功，当前进程ID:", cmd.Process.Pid)
+		cmd.Wait()
 	}
-	Logs.Loggers().Println("进程ID:", cmd.Process.Pid)
-	cmd.Wait()
 }
--- main.go
+++ main.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	fmt.Print("欢迎使用解析服务器客户端！！！")
 	UnityServer.InitClient()
-	UnityServer.TestRun()
+	UnityServer.Test2()
 }
