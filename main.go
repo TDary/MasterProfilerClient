b2144b9cff6611fba082398c446e27faf5433800
陈德睿 <w_chenderui@kingsoft.com>
2023-09-24 19:03:09 +0800

    日志初始化

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 )
 
 func main() {
+	//日志初始化
+	Logs.Init()
 	Logs.Loggers().Print("欢迎使用解析服务器客户端！！！")
 	clientUrl := UnityServer.InitClient()
 	go ParseServer.AnalyzeRangeCheck() //检测解析任务进行启动解析
